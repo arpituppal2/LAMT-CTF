9378481b3c4ad4086e63a47c498eddccd02913d4
Arpit Uppal <someone040807@gmail.com>
2026-03-07 19:31:23 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import { Inter } from 'next/font/google'
+import './globals.css'
+
 const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'LAMT-CTF',
+  description: 'High school math CTF for LAMT',
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
